a4187449f274cde1fc78c91c0fe1478f1f1895cb
lucas <lucascamposfrr@gmail.com>
2025-11-05 23:22:54 -0300

    att

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,16 +4,10 @@
 vim.keymap.set("n", "<leader>l", "<cmd>bn<CR>", { noremap = true, silent = true, desc = "go to next buffer" })
 vim.keymap.set("n", "<leader>h", "<cmd>bp<CR>", { noremap = true, silent = true, desc = "go to previous buffer" })
 
--- Remapeia o ; para entrar no Modo de Comando (:)
 vim.keymap.set("n", ";", ":", { noremap = true, silent = true, desc = "Comando" })
 
--- Opcional, mas recomendado: Mapear o : de volta para a função original do ;
--- (Repetir o último f, t, F ou T)
 vim.keymap.set("n", ":", ";", { noremap = true, silent = true, desc = "Repetir busca de caractere" })
 
--- Mapeia 'jk' para <Esc> no Modo de Inserção
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Sair do Modo Inserção" })
 
-
--- Opcional: Mapear 'kj' também
 vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true, desc = 'Sair do Modo Inserção' })
