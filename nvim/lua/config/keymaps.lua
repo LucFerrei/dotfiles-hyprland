9a4cfc6703e9d7be8fffffd627f261c8d17c8775
lucas <lucascamposfrr@gmail.com>
2025-12-18 12:10:40 -0300

    sync with thinkpad

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,6 +4,9 @@
 vim.keymap.set("n", "<leader>l", "<cmd>bn<CR>", { noremap = true, silent = true, desc = "go to next buffer" })
 vim.keymap.set("n", "<leader>h", "<cmd>bp<CR>", { noremap = true, silent = true, desc = "go to previous buffer" })
 
+vim.keymap.set("n", "o", "o<Esc>", { noremap = true, silent = true, desc = "create new line beyond" })
+vim.keymap.set("n", "O", "O<Esc>", { noremap = true, silent = true, desc = "create new line beyond" })
+
 vim.keymap.set("n", ";", ":", { noremap = true, silent = true, desc = "Comando" })
 
 vim.keymap.set("n", ":", ";", { noremap = true, silent = true, desc = "Repetir busca de caractere" })
@@ -12,4 +15,5 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, desc = "Sair
 
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true, desc = "Sair do Modo Inserção" })
 
+vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true, desc = "Toggle Neo-tree" })
 vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", { silent = true, desc = "Toggle Neo-tree" })
