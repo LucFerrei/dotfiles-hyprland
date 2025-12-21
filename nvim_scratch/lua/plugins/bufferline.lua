b02528fc95485551818395b3c7ec3fd94f805e7c
lucas <lucascamposfrr@gmail.com>
2025-12-21 06:23:35 -0300

    feat: fixing bufferline style and colors

--- nvim_scratch/lua/plugins/bufferline.lua
+++ nvim_scratch/lua/plugins/bufferline.lua
@@ -5,12 +5,41 @@ return{
 	dependencies = 'nvim-tree/nvim-web-devicons',
 	opts = {
 	    options = {
-		separator_style = "slant",
+		indicator = {
+		    style = 'underline',
+		},
+		buffer_close_icon = '󰅖',
+		modified_icon = '● ',
+		close_icon = ' ',
+		left_trunc_marker = ' ',
+		right_trunc_marker = ' ',
+		separator_style = {'', ''},
 		show_buffer_close_icons = false,
 		show_close_icon = false,
 		diagnostics = "nvim_lsp",
 		always_show_bufferline = true,
-	    }
+	    },
+	    highlights = {
+		indicator_selected = {
+		    fg = '#9ccfd8',
+		},
+		buffer_selected = {
+		    sp = '#9ccfd8',
+		    underline = true,
+		},
+		separator = {
+		    fg = '#232136',
+		    bg = '#232136',
+		},
+		separator_selected = {
+		    fg = '#232136',
+		    bg = '#232136',
+		},
+		separator_visible = {
+		    fg = '#232136',
+		    bg = '#232136',
+		},
+	    },
 	}
     }
 }
