80497db5693b6d5a124ee3accd165c7fafc13168
lucas <lucascamposfrr@gmail.com>
2026-01-23 18:35:06 -0300

    .

--- nvim_scratch/lua/config/keymaps.lua
+++ nvim_scratch/lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 vim.keymap.set("n", ";", ":", { noremap = true, })
 --- moves
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true, })
--- vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true, })
+vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true, })
 --- move lines (insert mode)
 vim.keymap.set("n", "m", "<Cmd>m+1<CR>", { noremap = true, silent = true, })
 vim.keymap.set("n", "M", "<Cmd>m-2<CR>", { noremap = true, silent = true, })
